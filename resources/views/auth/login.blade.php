cccf66db5392727ffb242b62759b89e2741cc8c5
ddiskandar <dd@smkplusalfarhan.sch.id>
2021-07-18 04:48:05 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
         <div class="flex flex-col flex-1 min-h-screen pt-12 bg-gray-100 sm:justify-center sm:pt-0">
             <div class="w-full px-12 lg:w-96 ">
                 <div class="pb-12 lg:hidden">
-                    <a href="/" class="flex items-center text-sm font-semibold text-indigo-500 uppercase ">
+                    <a href="https://smkplusalfarhan.sch.id" class="flex items-center text-sm font-semibold text-indigo-500 uppercase ">
                         <svg class="w-6 h-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 17l-5-5m0 0l5-5m-5 5h12" />
                         </svg>
